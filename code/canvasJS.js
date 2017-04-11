00947efebd4bc6bb641af06c4a7eb20dd4dbfe0d
Btripp75 <Brendontripp513@gmail.com>
2017-04-11 08:43:55 -0400

    Create canvasJS.js

---
+++ code/canvasJS.js
@@ -0,0 +1,16 @@
+c = document.createElement('canvas');
+c.width = 800;
+c.height = 600;
+c;
+document.body.appendChild(c);
+ctx = c.getContext('2d');
+ctx.fillRect(0,0,800,600);
+ctx.fillStyle = 'purple';
+ctx.fillRect(20,20,200,200);
+ctx.fillStyle = 'blue';
+ctx.fillStyle(40,40,160,160);
+ctx.fillRect(40,40,160,160);
+ctx.fillStyle = 'green';
+ctx.fillRect(60,60,120,120);
+ctx.beginPath();
+ctx.closePath();
